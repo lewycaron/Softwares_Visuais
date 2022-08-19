6ef9f64da49556d0e6fa2b11b53cb4da2515e838
LewyCaron <lewycaron@gmail.com>
2022-08-18 22:38:03 -0300

    Add files via upload

---
+++ Desenvolvimento de software Visual/Aula_02/Program.cs
@@ -0,0 +1,103 @@
+﻿using System
+
+namespace Aula02{
+    
+    class Program{
+        static void Main(string[] args)
+        {
+           
+            Console.WriteLine("Escolha um exercicio entrer 1 e 6");
+            Console.WriteLine("1 - Calculo de area");
+            Console.WriteLine("2 - Conversor de moeda");
+            Console.WriteLine("3 - Identificador de Idade");
+            Console.WriteLine("4 - Sequencia Fibonnacci");
+            Console.WriteLine("5 - ");
+            Console.WriteLine("6 - ");
+
+            double menu = Convert.ToDouble(Console.ReadLine()); 
+
+            switch (menu){
+
+                case 1:
+                    Console.WriteLine("Escreva a Largura: ");
+                    double largura = Convert.ToDouble(Console.ReadLine());   
+                    Console.WriteLine("Escreva a Altura: ");
+                    double altura = Convert.ToDouble(Console.ReadLine());
+                    double area = largura * altura;
+                    Console.WriteLine("O valor da área é: " +area+ "m²");                
+                break;
+                
+                case 2:
+                    Console.WriteLine("Insira o valor em Reais:");
+                    double real = Convert.ToDouble(Console.ReadLine());
+
+                    Console.WriteLine("Escolha uma opção de coversão!");
+                    Console.WriteLine("1 - Dolar");
+                    Console.WriteLine("2 - Euro");
+                    Console.WriteLine("3 - Peso Argentino");
+                    double opcao = Convert.ToDouble(Console.ReadLine());
+                    double valor = 0;
+                    switch(opcao){
+                        case 1:
+                            double dolar = 5.17;
+                            valor = real * dolar;
+                            System.Console.WriteLine("O valor convertido é de: U$ "+valor+ " Dolares");
+                        break;
+                        case 2:
+                            double euro = 5.17;
+                            valor = real * euro;
+                            System.Console.WriteLine("O valor convertido é de: E$ "+euro+ " Euros");
+                        break;
+
+                        case 3:
+                            double peso = 0.05;
+                            valor = real * peso;
+                            System.Console.WriteLine("O valor convertido é de: P$ " +peso+ " Pesos Argentinos");
+                        break;
+
+                        default:
+                            System.Console.WriteLine("Opção invalida");
+                        break;
+                    }
+                break;
+                case 3:
+
+                    System.Console.WriteLine("Informe a idade: ");
+                    int idade = Convert.ToInt16(Console.ReadLine());
+
+                    if (idade <= 13){
+                        System.Console.WriteLine("A idade de "+ idade + " é uma criança!");
+                    }else if(idade <= 18){
+                        System.Console.WriteLine("A idade de "+ idade + " é um(a) aborrecente.");
+                        }else if(idade <= 60){
+                            System.Console.WriteLine("A idade de "+ idade + " é um(a) adulto.");
+                        }else{
+                            System.Console.WriteLine("A idade de "+ idade + " é um(a) Idoso.");
+                        }
+                break;
+                case 4:
+
+                    System.Console.WriteLine("Insira o valor:");
+                    int rep = Convert.ToInt16(Console.ReadLine());
+                    int[] cont = {0};
+                    for (int i = 0; i <= rep; i++){
+                        
+                        if (cont[i] == 0){
+                            System.Console.WriteLine(cont[i]);
+                        }
+                        cont[i] = cont[i-1]+cont[i-2]
+                                               
+                    }
+
+                break;                
+
+                case 5:
+                break;
+                
+                default:
+                break;
+                    
+            }
+        }
+    }
+}
---
+++ Desenvolvimento de software Visual/Aula_03/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace Aula_03
+{
+    class Program
+    {
+        static double calculaMedia(double[] numeros){
+                       
+            double soma = 0;
+            for (int i = 0; i < numeros.Length; i++){
+                soma += numeros[i];
+            }
+            
+            return soma / numeros.Length;;
+        }
+
+        static double calcularMediana(double[] numeros){
+
+            Array.Sort(numeros);
+            int meio = numeros.Length / 2;
+           
+           if ((numeros.Length % 2) == 0){
+            return (numeros[meio] + numeros[meio-1]) /2 ;
+           }else{
+            return numeros[meio];
+           }
+        
+        }
+        static double calculaModa(double[] numeros){
+            
+            double[] repeticoes = new double[numeros.Length];
+
+            for (int i = 0; i < numeros.Length; i++){
+
+                double atual = numeros[i];
+                int cont = 0;
+                
+                for (int j = 0; j < numeros.Length; j++){
+                    if (numeros[j] == atual){
+                        cont++;
+                    }
+                }
+
+                repeticoes[i] = cont;
+            }
+
+            int maior = 0;
+            for (int i = 1; i < repeticoes.Length; i++)
+            {
+                if(repeticoes[i] > repeticoes[i-1]){
+                    maior = i;
+                }
+            }
+            return numeros[maior];
+        }
+
+        static void Main(string[] args){
+        
+            double[] numeros = {2, 7, 0, 7, 8, 9, 6, 8};
+
+            Console.WriteLine("A média é: "+calculaMedia(numeros));
+            Console.WriteLine("A mediana é: "+calcularMediana(numeros));
+            Console.WriteLine("A Moda é: "+ calculaModa(numeros));
+
+        }            
+    }
+}
