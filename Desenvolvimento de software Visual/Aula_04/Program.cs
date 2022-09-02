39a369f3d8e05eb53bb296613f563045ee42fad5
LewyCaron <lewycaron@gmail.com>
2022-09-01 22:32:40 -0300

    Add files via upload

---
+++ Desenvolvimento de software Visual/Aula_04/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace Aula_04
+{
+    class Program
+    {
+        static void Main(String[]args){
+            Usuario u = new Usuario("jango", "batatinha123");
+            Usuario p = new Usuario("ana", "aimeudeus");
+            Usuario q = new Usuario("Lewy", "pokemon38");
+            userBase myBase = new UserBase("userbase.txt");
+            myBase.AddUser(u);
+            myBase.AddUser(p);
+            myBase.AddUser(q);
+            myBase.Save();
+            //Console.WriteLine(u.Serialize());
+        }
+    }
+}
---
+++ Desenvolvimento de software Visual/Aula_04/Servicos.cs
@@ -0,0 +1,81 @@
+using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace Aula_04{
+
+    class Usuario{
+
+        static int UID = 0;
+        
+        int id;
+        string username;
+        string hash;
+
+        public Usuario(string u, string p){
+            
+            id = getUID();
+            SetUsername(u);
+            ApplyHash(p);
+
+        }
+
+        static int getUID(){
+            
+            UID++;
+            return UID;
+        }
+
+        public void ApplyHash(string password){
+            //TODO aplicar Hash.
+            hash = password;
+        }
+        
+        public void SetUsername(string u){
+            username = u;
+        }
+
+
+        public string getHash(){
+            return hash;
+        }
+
+        public string Serialize(){
+            return id + "," + username + ","+ hash;
+        }
+
+    }
+    class UserBase
+    {
+        string filename;
+        List<Usuario> usuarios;
+
+        public UserBase(){
+            filename = f;
+            usuarios = new List<Usuario>();
+            LoadUser();
+        }
+
+        void LoadUser(){
+            //TODO carregar usuarios do arquivo
+        }
+
+        public void AddUser(Usuario u)
+        {
+            usuarios.Add(u);
+        }
+
+        public void Save(){
+            string output = "";
+
+            foreach (Usuario u in usuarios)
+            {
+                output += u.Serialize() + "\n";
+            }
+
+            File.WriteAllText(filename, output);
+        }
+
+    }
+
+}
